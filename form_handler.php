ffcce4a5a3983b83fd7d883fb9f139cb1611d02c
Ravialli <74206232+allisonChengg@users.noreply.github.com>
2022-05-30 20:29:00 -0400

    Handles Info Sent from Contact Page

---
+++ form_handler.php
@@ -0,0 +1,21 @@
+<?php
+$name - $POST['name'];
+$visitor_email - $POST['email'];
+$subject - $POST['subject'];
+$message - $POST['message'];
+
+$email_from = 'woottontsa.org'; /*domain name*/
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".   
+              "Subject: $subject.\n".   
+              "User Message: $visitor_email.\n";
+
+$to = 'wootton.tsa@gmail.com';
+$headers = "From: $ email_from \r\n";
+$headers.= "Reply To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: contact.html");
+?>
